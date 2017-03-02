cc1e93065063a5f018d94cbeed765df9e05a96e1
Yamini Bolisetty <yamini.bolisetty@gmail.com>
2017-03-01 20:19:08 -0600

    additional files added

---
+++ src/main/java/com/mycompany/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.mycompany;
+
+/**
+ * Created by yami on 3/1/17.
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
