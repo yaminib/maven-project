e8218cfcccfd5bd15316c3ff8b359c61fe55919e
Yamini Bolisetty <yamini.bolisetty@gmail.com>
2017-03-01 21:05:38 -0600

    Adding MathOperations class

---
+++ src/main/java/com/mycompany/MathOperations.java
@@ -0,0 +1,12 @@
+package com.mycompany;
+
+/**
+ * Created by yami on 3/1/17.
+ */
+public class MathOperations {
+    public static void main(String[] args) {
+        int sum = 2+3;
+        int sub = 5-1;
+        System.out.println("The sum is " + sum + " and the difference is " + sub);
+    }
+}
